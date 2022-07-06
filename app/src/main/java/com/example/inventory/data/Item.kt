368269b5b9ba318c806b74fd02b097c6e1462856
Gabriel <gabriel99luques@gmail.com>
2022-07-06 16:41:29 -0300

    [FIX] Item - change name columnInfo

--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -5,14 +5,14 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 
-@Entity(tableName = "item")
-data class Item (
+@Entity
+data class Item(
     @PrimaryKey(autoGenerate = true)
-    val id : Int = 0,
-    @ColumnInfo(name = "info")
-    val itemName : String,
+    val id: Int = 0,
+    @ColumnInfo(name = "name")
+    val itemName: String,
     @ColumnInfo(name = "price")
-    val itemPrice : Double,
+    val itemPrice: Double,
     @ColumnInfo(name = "quantity")
-    val quantityInStock : Int
+    val quantityInStock: Int,
 )
